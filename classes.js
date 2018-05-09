17671138332943f036ea48a87e63569f2779db55
Riki Parata <you@example.com>
2018-05-10 00:32:26 +1200

    last colour

--- classes.js
+++ classes.js
@@ -26,5 +26,8 @@ function two () {
   two.classList.add('green')
 }
   // CREATE FUNCTION three HERE
-
+  function three () {
+    var three = document.getElementById('three')
+    three.classList.add('purple')
+  }
 // CREATE FUNCTION makeVisible HERE
